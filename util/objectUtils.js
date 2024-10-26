351cfb74a82618962687ce2525576ebbd64bf2bb
Alex Altieri <alexandra.m.altieri@gmail.com>
2024-10-26 09:33:11 -0700

    only compact undefined values

--- util/objectUtils.js
+++ util/objectUtils.js
@@ -1,4 +1,4 @@
 export const compact = obj =>
   Object.fromEntries(
-    Object.entries(obj).filter(([key, value]) => value !== undefined && value !== '')
+    Object.entries(obj).filter(([key, value]) => value !== undefined)
   );
